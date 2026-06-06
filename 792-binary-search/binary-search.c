729c791e6252eace43ac7725cf42e1340045da60
ankush5-5 <ankushsahab210@gmail.com>
2026-06-06 20:44:00 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (8.04%) - LeetSync

---
+++ 792-binary-search/binary-search.c
@@ -0,0 +1,18 @@
+int search(int* nums, int numsSize, int target) {
+    int left = 0;
+    int right = numsSize - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;  // avoid overflow
+
+        if (nums[mid] == target) {
+            return mid;  // found target
+        } else if (nums[mid] < target) {
+            left = mid + 1;  // search right half
+        } else {
+            right = mid - 1; // search left half
+        }
+    }
+
+    return -1;  // not found
+}
